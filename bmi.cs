6fc2910414c007f5be9e0fb82180ca8a033fb892
Ngoc Vinh <phvinh1206@gmail.com>
2026-05-08 22:18:52 +0700

    first commit

---
+++ bmi.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Dynamic;
+
+class BMI
+{
+    // field
+    private double chieuCao;
+    private double canNang;
+
+    // property
+    public double ChieuCao
+    {
+        get { return chieuCao; }
+        set { chieuCao = value; }
+    }
+
+    public double CanNang
+    {
+        get { return canNang; }
+        set { canNang = value; }
+    }
+
+    // constructor rỗng
+    public BMI()
+    {
+    }
+
+    // constructor có tham số
+    public BMI(double a, double b)
+    {
+        ChieuCao = a;
+        CanNang = b;
+    }
+
+    // hàm tính BMI
+    public void TinhBMI()
+    {
+        Console.Write("Nhap chieu cao (m): ");
+        ChieuCao = double.Parse(Console.ReadLine());
+
+        Console.Write("Nhap can nang (kg): ");
+        CanNang = double.Parse(Console.ReadLine());
+
+        double bmi = CanNang / (Math.Pow(ChieuCao, 2));
+
+        Console.WriteLine("BMI = " + bmi);
+
+        if (bmi < 18.5)
+            Console.WriteLine("Gay");
+        else if (bmi < 25)
+            Console.WriteLine("Binh thuong");
+        else
+            Console.WriteLine("Thua can");
+    }
+
+    // hàm main
+    static void Main(string[] args)
+    {
+        BMI b = new BMI();
+        b.TinhBMI();
+    }
+}
---
+++ checkMonth.cs
@@ -0,0 +1,36 @@
+class checkeMonth
+{
+
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("nhap thang");
+        int month = int.Parse(Console.ReadLine());
+        while (month < 1 || month > 12)
+        {
+            System.Console.WriteLine("thang khong dươc be hon 1 va lon hon 12!! Vui long nhap lai:");
+            month = int.Parse(Console.ReadLine());
+
+        }
+        System.Console.WriteLine("nhap nam:");
+        int nam = int.Parse(Console.ReadLine());
+
+        if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 | month == 12)
+        {
+            System.Console.WriteLine($"thang {month} co 31 ngay");
+        }
+
+        else if ((month == 2 && nam % 4 == 0 && nam % 100 != 0) || (month == 2 && nam % 400 == 0))
+        {
+            Console.WriteLine($"thang {month} co 29 ngay");
+        }
+        else if (month == 2)
+        {
+            Console.WriteLine($"thang {month} co 28 ngay");
+
+        }
+        else
+        {
+            System.Console.WriteLine($"thang {month} co 30 ngay");
+        }
+    }
+}
