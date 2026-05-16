1abfa2824591febb43822033d93d0fb30f9fe27f
Ngoc Vinh <phvinh1206@gmail.com>
2026-05-17 02:05:41 +0700

    Chuoikitu.cs

--- baitapcs/Chuoikitu.cs
+++ baitapcs/Chuoikitu.cs
@@ -64,6 +64,47 @@ public class Chuoikitu
                 //     System.Console.WriteLine("chuoi 1 va chuoi 2 khac nhau");
                 // }
 
-                
-        }
+                // string s = "    hello world    ";
+                // System.Console.WriteLine(s.Length);//do dai chuoi
+                // System.Console.WriteLine(s.IndexOf("e"));//tim gio tri trong chuoi tra ve vi tri index
+                // System.Console.WriteLine($"{string.Join(" ,", s)}");
+                // bool kt = s.StartsWith("h");
+                // System.Console.WriteLine(kt);// kiem tra chuoi co bat daua bang tu ben trong hay khong
+
+                // string s1 = s.Substring(3, 2);//trich tu chuoi ban dau tu vi tri thu index va lay count ki tu
+                // System.Console.WriteLine(s1);
+                // System.Console.WriteLine(s.ToUpper());//tra ve kieu in hoa
+                // System.Console.WriteLine(s.ToLower());///tra chuoi ve chu thuong
+                // System.Console.WriteLine(s.Trim());//xoa khoang trang dau va cuoi:trimStart()xoa khoang trang dau chuoi trimEnd() xoa khoang trang cuoi chuoi
+                // string[] tachchuoi = s.Split(' '); //trong (_) las dk tach
+                // foreach (string i in tachchuoi)
+                // {
+                //         System.Console.WriteLine(i);
+                // }
+                // System.Console.WriteLine($"{string.Join("-", tachchuoi)}");//gop chuoi 
+//baitap1 tinh tong cua cac mon hoc
+        //         string str1 = "English= 78 Scinece= 83 Math= 68 History= 65";
+        //         string[] str2 = str1.Split(' ');
+        //         string str3 = "";
+        //         foreach (string i in str2)
+        //         {
+        //                 foreach (char c in i)
+        //                 {
+        //                         if (char.IsDigit(c))
+        //                         {
+        //                                 str3 += " " + i;
+        //                                 break;
+        //                         }
+        //                 }
+        //         }
+        //         //System.Console.WriteLine(str3);
+        //         string[] str4 = str3.Trim().Split(" ");
+        //         int sum = 0;
+        //         foreach (string i in str4)
+        //         {
+        //                 sum += int.Parse(i);
+        //         }
+        //         System.Console.Write($"{string.Join("+", str4)}");
+        //         System.Console.WriteLine($"={sum}");
+        // }
 }
