24cae0ba1ecf434a1d18c6a02c74a36e7f59fbca
Ngoc Vinh <phvinh1206@gmail.com>
2026-05-17 00:50:15 +0700

    update chuoikitu

--- baitapcs/Chuoikitu.cs
+++ baitapcs/Chuoikitu.cs
@@ -1,68 +1,69 @@
-// using System;
+using System;
 
-// namespace C_.baitapcs;
+namespace C_.baitapcs;
 
-// public class Chuoikitu
-// {
-//     static void Main(string[] args)
-//     {
-        // string s = "day la chuoi ki tu";
-        // string ss="@hello \n toi ten vinh \t nam nay toi 20 tuoi \\ hoc tai dai hoc sai gon \' \" ";
-        // System.Console.WriteLine(s);
-        // System.Console.WriteLine(ss);
+public class Chuoikitu
+{
+        static void Main(string[] args)
+        {
+                // string s = "day la chuoi ki tu";
+                // string ss="@hello \n toi ten vinh \t nam nay toi 20 tuoi \\ hoc tai dai hoc sai gon \' \" ";
+                // System.Console.WriteLine(s);
+                // System.Console.WriteLine(ss);
 
-        // //tach chuoi thanh ki tu le
-        // string s1="hello world";
-        // System.Console.WriteLine(s1.Length);// do dai chuoi
-        // char[] lst=s1.ToCharArray();
-        // System.Console.WriteLine(lst);
+                // //tach chuoi thanh ki tu le
+                // string s1="hello world";
+                // System.Console.WriteLine(s1.Length);// do dai chuoi
+                // char[] lst=s1.ToCharArray();
+                // System.Console.WriteLine(lst);
 
-        //bai tap
-        // System.Console.WriteLine("nhap vao chuoi ki tu: ");
-        // string s = Console.ReadLine();
-        // char[] lst = s.ToCharArray();//chuyen doi thanh mang ki tu
-        // int ChuThuong = 0;
-        // int chuHoa = 0;
-        // int so = 0;
-        // int space = 0;
-        // foreach (char c in lst)
-        // {
-        //     if (char.IsLower(c) == true)
-        //     {
-        //         ChuThuong++;
-        //     }
+                //bai tap
+                // System.Console.WriteLine("nhap vao chuoi ki tu: ");
+                // string s = Console.ReadLine();
+                // char[] lst = s.ToCharArray();//chuyen doi thanh mang ki tu
+                // int ChuThuong = 0;
+                // int chuHoa = 0;
+                // int so = 0;
+                // int space = 0;
+                // foreach (char c in lst)
+                // {
+                //     if (char.IsLower(c) == true)
+                //     {
+                //         ChuThuong++;
+                //     }
 
-        //     if (char.IsUpper(c) == true)
-        //     {
-        //         chuHoa++;
-        //     }
-        //     if (char.IsDigit(c) == true)
-        //     {
-        //         so++;
-        //     }
-        //     if (char.IsWhiteSpace(c) == true)
-        //     {
-        //         space++;
-        //     }
-        // }
-        // System.Console.WriteLine($"Co {ChuThuong} chu thuong");
-        // System.Console.WriteLine($"Co {chuHoa} chu hoa");
-        // System.Console.WriteLine($"Co {so} so");
-        // System.Console.WriteLine($"Co {space} space");
+                //     if (char.IsUpper(c) == true)
+                //     {
+                //         chuHoa++;
+                //     }
+                //     if (char.IsDigit(c) == true)
+                //     {
+                //         so++;
+                //     }
+                //     if (char.IsWhiteSpace(c) == true)
+                //     {
+                //         space++;
+                //     }
+                // }
+                // System.Console.WriteLine($"Co {ChuThuong} chu thuong");
+                // System.Console.WriteLine($"Co {chuHoa} chu hoa");
+                // System.Console.WriteLine($"Co {so} so");
+                // System.Console.WriteLine($"Co {space} space");
 
-        //so sanh 2 chuoi
-        // string chuoi1 = "hello";
-        // string chuoi2 = "helloo";
-        // string chuoi3 = "hello";
+                //so sanh 2 chuoi
+                // string chuoi1 = "hello";
+                // string chuoi2 = "helloo";
+                // string chuoi3 = "hello";
 
-        // if (chuoi1.CompareTo(chuoi3)==0)
-        // {
-        //     System.Console.WriteLine("chuoi 1 = chuoi 3");
-        // }
-        // if (chuoi1.CompareTo(chuoi2) != 0)
-        // {
-        //     System.Console.WriteLine("chuoi 1 va chuoi 2 khac nhau");
-        // }
+                // if (chuoi1.CompareTo(chuoi3)==0)
+                // {
+                //     System.Console.WriteLine("chuoi 1 = chuoi 3");
+                // }
+                // if (chuoi1.CompareTo(chuoi2) != 0)
+                // {
+                //     System.Console.WriteLine("chuoi 1 va chuoi 2 khac nhau");
+                // }
 
-//     }
-// }
+                
+        }
+}
