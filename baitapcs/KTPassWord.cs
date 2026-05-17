c235f2e7b30ea10318aca2e1456bf11e7e60f7dc
Ngoc Vinh <phvinh1206@gmail.com>
2026-05-18 01:53:39 +0700

    ktpw2

--- baitapcs/KTPassWord.cs
+++ baitapcs/KTPassWord.cs
@@ -8,9 +8,15 @@ namespace C_.baitapcs
 {
     public class KTPassWord
     {
-        public static void ktChuSo(string a)
+        public static string thietlappw()
         {
-            string mk = "Pnv12345";
+            System.Console.WriteLine("Moi ban nhap mat khau cua ban(mat khau phai co chu, co so vaf tu 6 ki tu tro len)");
+            string s = Console.ReadLine();
+            System.Console.WriteLine("Ban da thiet lap mat khau thanh cong");
+            return s;
+        }
+        public static void ktChuSo(string a, string s)
+        {
             int so = 0;
             int chu = 0;
             int dem = 0;
@@ -29,20 +35,23 @@ namespace C_.baitapcs
                     }
 
                 }
-                if (chu * so != 0 && a.Length >= 6 || a.Equals(mk))
+                if (chu * so != 0 && a.Length >= 6 && a.Equals(s))
                 {
-                    System.Console.WriteLine("mat khau hop le");
+                    System.Console.WriteLine("Dang nhap thanh cong");
                     break;
                 }
                 else
                 {
-                    System.Console.WriteLine("sai mat khau");
+                    System.Console.WriteLine($"Ban da nhap sai mat khau {dem}/5 lan! ");
+                    System.Console.WriteLine("============================");
+                    System.Console.WriteLine("nhap lai mat khau");
+                    a = Console.ReadLine();
                     dem++;
                 }
-                System.Console.WriteLine($"moi ban nhap lai lan thu {dem} :");
-                a = Console.ReadLine();
+
                 if (dem == 5)
                 {
+                    System.Console.WriteLine($"ban da nhap mat khau sai qua {dem} lan");
                     check = false;
                 }
             }
@@ -50,9 +59,12 @@ namespace C_.baitapcs
 
         static void Main(string[] args)
         {
-            System.Console.WriteLine("nhap mat khau");
-            string? ip = Console.ReadLine();
-            ktChuSo(ip);
+            string s = thietlappw();
+            System.Console.WriteLine("======================================");
+            System.Console.WriteLine("================Login=================");
+            System.Console.WriteLine("Nhap mat khau dang nhap:");
+            string ip = Console.ReadLine();
+            ktChuSo(ip, s);
         }
 
     }
