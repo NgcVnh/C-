fcd45898718058c2a1d7df46861cb6083e1a779e
Ngoc Vinh <phvinh1206@gmail.com>
2026-05-21 01:30:51 +0700

    OOP

---
+++ OOP/main.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace C_.OOP
+{
+    public class main
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("hello world");
+        }
+    }
+}
